83ce040e2dc38d54b557bace40e2a226c5eeea52
V1ld3 <72020103+V1ld3@users.noreply.github.com>
2022-05-20 15:10:37 +0200

    created js.js

---
+++ html/Js/js.js
@@ -0,0 +1,8 @@
+function hideTopNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
