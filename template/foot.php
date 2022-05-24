0f83ab9edf5206de3d4d56c373dc5be272e69e9d
V1ld3 <72020103+V1ld3@users.noreply.github.com>
2022-05-24 10:48:39 +0200

    hu

--- template/foot.php
+++ template/foot.php
@@ -5,5 +5,5 @@
 
 $message = " Link to school website: <a href='https://www.ntigymnasiet.se/sodertalje/' title='Nti '>Nti Södertälje</a>";
 echo "Copyright &copy; 2022-" . date("Y") . $message;
-
+?>
 <script src="js/js.js"></script>
